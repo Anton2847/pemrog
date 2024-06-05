881b768f006782e4cd0c87c2d94b73b92df21e12
Anton2847 <127730538+Anton2847@users.noreply.github.com>
2024-06-05 14:00:35 +0700

    Add files via upload

---
+++ uas3/uas3/Program.cs
@@ -0,0 +1,76 @@
+﻿// Interface IEat
+public interface IEat
+{
+    void Antre();
+    void Bayar();
+}
+
+// Kelas Dine yang mengimplementasikan interface IEat
+public class Dine : IEat
+{
+    public void Antre()
+    {
+        Console.WriteLine("Antre ambil makanan di tempat yang disediakan.");
+    }
+
+    public void Bayar()
+    {
+        Console.WriteLine("Bayar makanan sesudah selesai makan.");
+    }
+}
+
+// Kelas Take yang mengimplementasikan interface IEat
+public class Take : IEat
+{
+    public void Antre()
+    {
+        Console.WriteLine("Antre pesan makanan di tempat yang disediakan.");
+    }
+
+    public void Bayar()
+    {
+        Console.WriteLine("Langsung bayar makanan.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Selamat datang di RM Padang Benderang");
+        Console.WriteLine("Pilih opsi makan:");
+        Console.WriteLine("1. Makan di Tempat");
+        Console.WriteLine("2. Bungkus");
+        Console.Write("Masukkan pilihanmu: ");
+        int opsi = Convert.ToInt32(Console.ReadLine());
+
+        IEat eat;
+
+        // Percabangan
+        if (opsi == 1)
+        {
+            eat = new Dine();
+        }
+        else if (opsi == 2)
+        {
+            eat = new Take();
+        }
+        else
+        {
+            Console.WriteLine("Pilihan tidak ada.");
+            return;
+        }
+
+        Console.WriteLine();
+        // Memanggil metode Antre
+        eat.Antre();
+
+        // Memanggil metode Bayar
+        eat.Bayar();
+
+        Console.WriteLine();
+        Console.WriteLine("Terimakasih sudah datang.");
+
+        Console.ReadLine();
+    }
+}
---
+++ uas4/uas4/Program.cs
@@ -0,0 +1,59 @@
+﻿// Masak sebagai kelas abstrak
+public abstract class Masak
+{
+    // Proses sebagai metode abstrak
+    public abstract void Proses();
+
+    // Kegiatan sebagai metode konkret
+    public void Kegiatan()
+    {
+        Console.WriteLine("Makanan sedang dimasak.");
+    }
+}
+
+// Subclass Rebus yang mewarisi dari kelas abstrak Masak
+public class Rebus : Masak
+{
+    // Implementasi metode abstrak Proses
+    public override void Proses()
+    {
+        Console.WriteLine("Masakan sedang direbus.");
+    }
+}
+
+// Subclass Goreng yang mewarisi dari kelas abstrak Masak
+public class Goreng : Masak
+{
+    // Implementasi metode abstrak Proses
+    public override void Proses()
+    {
+        Console.WriteLine("Masakan sedang digoreng.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Membuat objek dari kelas turunan Rebus
+        Rebus rebus = new Rebus();
+
+        // Memanggil metode Proses
+        rebus.Proses();
+
+        // Memanggil metode yang diwarisi dari kelas Masak
+        rebus.Kegiatan();
+
+        Console.WriteLine();
+        // Membuat objek dari kelas turunan Goreng
+        Goreng goreng = new Goreng();
+
+        // Memanggil metode Proses
+        goreng.Proses();
+
+        // Memanggil metode yang diwarisi dari kelas Masak
+        goreng.Kegiatan();
+
+        Console.ReadLine();
+    }
+}
